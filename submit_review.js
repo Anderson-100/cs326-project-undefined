0a41b6da37ad93fdcec111469f5d2068e60b1bee
Anderson Hsiao <phsiao@umass.edu>
2022-08-08 00:11:10 -0400

    added second question, reformatted class structure, fixed radio buttons

--- submit_review.js
+++ submit_review.js
@@ -1,42 +1,45 @@
 class App {
   constructor() {
     this.gradeQuestion = new GradeQuestion();
+    this.ratingQuestion = new RatingQuestion();
   }
 
   render() {
     const element = document.createElement('div');
 
     element.appendChild(this.gradeQuestion.render());
+    element.appendChild(this.ratingQuestion.render());
 
     return element;
   }
 }
 
-class GradeQuestion {
+class MultipleChoiceQuestion {
   constructor() {
     this.break = new LineBreak();
   }
 
-  render() {
+  render(name, question, options) {
     const div = document.createElement('div');
 
     const title = document.createElement('h4');
-    title.textContent = "What grade did you receive in this course?";
+    title.textContent = question;
     div.appendChild(title);
 
     const form = document.createElement('form');
-    const grades = ["A","A-","B+","B","B-","C+","C","C-","D+","D","D-","F"];
-    grades.forEach(grade => {
+
+    options.forEach(option => {
       const input = document.createElement('input');
       input.type = 'radio';
-      input.id = grade;
+      input.id = option;
       input.class = "multiple-choice-question";
-      input.value = grade;
+      input.value = option;
+      input.name = name;
       form.appendChild(input);
 
       const label = document.createElement('label');
-      label.for = grade;
-      label.textContent = grade;
+      label.htmlFor = option;
+      label.textContent = option;
       form.appendChild(label);
 
       // form.appendChild(this.break.render());
@@ -44,6 +47,34 @@ class GradeQuestion {
 
     div.appendChild(form);
     return div;
+  }
+}
+
+class GradeQuestion {
+  constructor() {
+    this.mcq = new MultipleChoiceQuestion();
+  }
+
+  render() {
+    return this.mcq.render(
+      "grade",
+      "What grade did you recieve in this course?",
+      ["A","A-","B+","B","B-","C+","C","C-","D+","D","D-","F"]
+    );
+  }
+}
+
+class RatingQuestion {
+  constructor() {
+    this.mcq = new MultipleChoiceQuestion();
+  }
+
+  render() {
+    return this.mcq.render(
+      "rating",
+      "What rating would you give this course overall? (1 is worst, 5 is best)",
+      ["1","2","3","4","5"]
+    );
   }
 }
 
