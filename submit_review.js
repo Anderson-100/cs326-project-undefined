3dededd6d65500c9ddf027b833a7460daa1e2f20
Anderson Hsiao <phsiao@umass.edu>
2022-08-08 00:58:24 -0400

    added text box for written review and submit button

--- submit_review.js
+++ submit_review.js
@@ -1,14 +1,30 @@
 class App {
   constructor() {
     this.gradeQuestion = new GradeQuestion();
+    this.diffQuestion = new DifficultyQuestion();
     this.ratingQuestion = new RatingQuestion();
+    this.reviewText = new ReviewTextBox();
+    this.buttons = new ButtonsDiv();
+    this.break = new LineBreak();
   }
 
   render() {
+    document.title = "Submit a Review - UMass Course Review "
     const element = document.createElement('div');
 
     element.appendChild(this.gradeQuestion.render());
+    element.appendChild(this.break.render());
+
+    element.appendChild(this.diffQuestion.render());
+    element.appendChild(this.break.render());
+
     element.appendChild(this.ratingQuestion.render());
+    element.appendChild(this.break.render());
+
+    element.appendChild(this.reviewText.render());
+    element.appendChild(this.break.render());
+
+    element.appendChild(this.buttons.render());
 
     return element;
   }
@@ -75,6 +91,60 @@ class RatingQuestion {
       "What rating would you give this course overall? (1 is worst, 5 is best)",
       ["1","2","3","4","5"]
     );
+  }
+}
+
+class DifficultyQuestion {
+  constructor() {
+    this.mcq = new MultipleChoiceQuestion();
+  }
+
+  render() {
+    return this.mcq.render(
+      "rating",
+      "How difficult was this course? (1 is easy, 5 is hard)",
+      ["1","2","3","4","5"]
+    );
+  }
+}
+
+class ReviewTextBox {
+  render() {
+    const div = document.createElement('div');
+
+    const title = document.createElement('h4');
+    title.textContent = "Type your review below:";
+    div.appendChild(title);
+
+    const textarea = document.createElement('textarea');
+    textarea.id = 'review_text';
+    textarea.rows = 10;
+    textarea.cols = 50;
+
+    div.appendChild(textarea);
+    return div;
+  }
+}
+
+class ButtonsDiv {
+  constructor() {
+    this.submit = new SubmitButton();
+  }
+
+  render() {
+    const div = document.createElement('div');
+    div.appendChild(this.submit.render());
+    return div;
+  }
+}
+
+class SubmitButton {
+  render() {
+    const button = document.createElement('input');
+    button.type = 'button';
+    button.id = 'submit-review';
+    button.value = 'Submit Review';
+    return button;
   }
 }
 
